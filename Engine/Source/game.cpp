b043e349c491c2a3945ded67232ad40ae22e455a
Chaniwat <chaniwat.meranote@gmail.com>
2015-03-29 15:42:35 +0700

    Textrender is ready :3

--- Engine/Source/game.cpp
+++ Engine/Source/game.cpp
@@ -1,11 +1,11 @@
 #include "game.h"
 #include "resourcemanager.h"
 #include "spriterender.h"
-//#include "textrender.h"
+#include "textrender.h"
 //#include "sound.h"
 
-SpriteRender *Renderer;
-//TextRedner *text;
+SpriteRender *SpriteRenderer;
+TextRender *TextRenderer;
 
 Game::Game(GLuint width, GLuint heigth) : windowWidth(width), windowHeight(heigth) ,Currentlevel(MAIN_MENU), Currenttheme(0), score(0)
 {
@@ -14,7 +14,7 @@ Game::Game(GLuint width, GLuint heigth) : windowWidth(width), windowHeight(heigt
 
 Game::~Game()
 {
-    delete Renderer;
+    delete SpriteRenderer;
 }
 
 GLvoid Game::init()
@@ -32,17 +32,21 @@ GLvoid Game::init()
     glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(this->windowWidth), static_cast<GLfloat>(this->windowHeight), 0.0f, -1.0f, 1.0f);
     ResourceManager::GetShader("sprite").Use().SetMatrix4("projection", projection);
     // Set render-specific controls
-    Renderer = new SpriteRender(ResourceManager::GetShader("sprite"));
+    SpriteRenderer = new SpriteRender(ResourceManager::GetShader("sprite"));
+    TextRenderer = new TextRender(this->windowWidth, this->windowHeight);
+    TextRenderer->Load("../Font/supermarket.ttf", 128);
 }
 
 GLvoid Game::Render(GLfloat dt)
 {
-    Renderer->Draw(ResourceManager::GetTexture("menu_background") ,glm::vec2(0, 0), glm::vec2(this->windowWidth, this->windowHeight), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-    Renderer->Draw(ResourceManager::GetTexture("pawn1") ,glm::vec2(35, 42), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-    Renderer->Draw(ResourceManager::GetTexture("pawn3"), glm::vec2(535, 352), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-    Renderer->Draw(ResourceManager::GetTexture("pawn1"), glm::vec2(145, 215), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-    Renderer->Draw(ResourceManager::GetTexture("orange"), glm::vec2(((this->windowWidth / 2) - 50), ((this->windowHeight / 2) - 50)), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-    Renderer->Draw(ResourceManager::GetTexture("pawn3"), glm::vec2(685, 231), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-    Renderer->Draw(ResourceManager::GetTexture("pawn2"), glm::vec2(456, 565), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
-    Renderer->Draw(ResourceManager::GetTexture("pawn2"), glm::vec2(875, 555), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+    SpriteRenderer->Draw(ResourceManager::GetTexture("menu_background") ,glm::vec2(0, 0), glm::vec2(this->windowWidth, this->windowHeight), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+    SpriteRenderer->Draw(ResourceManager::GetTexture("pawn1") ,glm::vec2(35, 42), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+    SpriteRenderer->Draw(ResourceManager::GetTexture("pawn3"), glm::vec2(535, 352), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+    SpriteRenderer->Draw(ResourceManager::GetTexture("pawn1"), glm::vec2(145, 215), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+    SpriteRenderer->Draw(ResourceManager::GetTexture("orange"), glm::vec2(((this->windowWidth / 2) - 50), ((this->windowHeight / 2) - 50)), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+    SpriteRenderer->Draw(ResourceManager::GetTexture("pawn3"), glm::vec2(685, 231), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+    SpriteRenderer->Draw(ResourceManager::GetTexture("pawn2"), glm::vec2(456, 565), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+    SpriteRenderer->Draw(ResourceManager::GetTexture("pawn2"), glm::vec2(875, 555), glm::vec2(100, 100), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+
+    TextRenderer->RenderText("Everyone love mimi <3", 250.0f, 200.0f, 0.40f, glm::vec3(255.0f / 255.0f, 28.0f / 255.0f, 202.0f / 255.0f));
 }
